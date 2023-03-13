6517d17033a698fbf6464daded09593a8c6a2f2d
Hugossj3 <gdhugo666@gmail.com>
2023-03-13 11:28:36 +0100

    bucle

--- src/main/java/com/nilhcem/fakesmtp/model/EmailModel.java
+++ src/main/java/com/nilhcem/fakesmtp/model/EmailModel.java
@@ -64,6 +64,10 @@ public final class EmailModel {
 	}
 	public static void main(String[] args) {
 		System.out.println("hola mundo");
+		for(int i=0;i<5;i++){
+			System.out.println(i);
+		}
+		
 	}
 }
 
